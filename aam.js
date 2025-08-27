960809d40576067d413abbd0b203466442997d08
FinlayWSCW <159732365+Watsonfi1@users.noreply.github.com>
2025-08-27 22:29:40 +1200

    Both selectors working separately from each other

--- aam.js
+++ aam.js
@@ -1,14 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const triggerEvent = document.querySelector('.triggerButton');
+    let currentMissile = null;
+    let currentPlane = null;
+    const planeEvent = document.querySelector('.planeButton');
+    const weaponEvent = document.querySelector('.weaponButton');
     const options =  document.querySelector('.options');
-    triggerEvent.addEventListener('click', () =>{
-        console.log('clicking')
-        options.classList.toggle('show')
+    const weaponOptions = document.querySelector('.weaponOptions');
+    planeEvent.addEventListener('click', () =>{
+        console.log('clicking');
+        options.classList.toggle('show');
         //if an item is selected then close the menu automatically
     });
     options.querySelectorAll('.option').forEach(option => {
         option.addEventListener('click', () => {
-            triggerEvent.innerHTML = option.innerHTML;
+            if (currentPlane != null){currentPlane.classList.toggle('hide')};
+            planeEvent.innerHTML = option.innerHTML;
+            options.classList.toggle('show');
+            console.log(option);
+            currentPlane = option; //CurrentPlane = 'Su27'//
+            option.classList.toggle('hide');
+        });
+    });
+    weaponEvent.addEventListener('click', () =>{
+        console.log('clicking');
+        weaponOptions.classList.toggle('show');
+        //if an item is selected then close the menu automatically
+    });
+    weaponOptions.querySelectorAll('.option').forEach(option => {
+        option.addEventListener('click', () => {
+            if (currentMissile != null){currentMissile.classList.toggle('hide')};
+            weaponEvent.innerHTML = option.innerHTML;
+            weaponOptions.classList.toggle('show');
+            console.log(option);
+            currentMissile = option; //CurrentPlane = 'Su27'//
+            option.classList.toggle('hide');
         });
     });
 });
