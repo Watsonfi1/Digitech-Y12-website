d6f743010daded3a636c1e8002b08224556ec51b
FinlayWSCW <159732365+Watsonfi1@users.noreply.github.com>
2025-08-29 22:16:01 +1200

    Air To Air Weapons Page Finished

--- aam.js
+++ aam.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const options =  document.querySelector('.options');
     const weaponOptions = document.querySelector('.weaponOptions');
     const weaponOptionsOptions = weaponOptions.querySelectorAll('.option');
+    weaponEvent.disabled = true; //FIX this it's not working like it was earlier?
     planeEvent.addEventListener('click', () =>{
         console.log('clicking');
         options.classList.toggle('show');
@@ -18,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
             // This doesn't work for some reason?  currentMissile.classList.toggle('hide');
             weaponEvent.innerHTML = defaultInner
             if (currentPlane != null){
-                currentPlane.classList.toggle('hide')
+                currentPlane.classList.toggle('hide');
                 selectedPWList.forEach(item =>{
                     item.classList.remove('hide');
                 });
@@ -28,16 +29,20 @@ document.addEventListener('DOMContentLoaded', () => {
             options.classList.toggle('show');
             currentPlane = option; //CurrentPlane = 'Su27'//
             option.classList.toggle('hide');
+            weaponEvent.disabled = false;
             weaponOptionsOptions.forEach(option2 =>{
                 selectedPlane = currentPlane?.dataset?.plane?.trim();
                 missileCheck = option2.dataset.plane?.trim();
                 if (selectedPlane && missileCheck.includes(selectedPlane)){
                     console.log("Somethings working");
+                    console.log(option2);
+                    option2.classList.remove('hide');
                     //It shows all the options including the one I have selected.// 
                 }
                 else if(selectedPlane){
                     option2.classList.add('hide');
                     selectedPWList.push(option2);
+                    console.log(option2);
                 }
             });
         });
@@ -45,11 +50,10 @@ document.addEventListener('DOMContentLoaded', () => {
     weaponEvent.addEventListener('click', () =>{
         console.log('clicking');
         weaponOptions.classList.toggle('show');
-        //if an item is selected then close the menu automatically
     });
     weaponOptionsOptions.forEach(option => {
         option.addEventListener('click', () => {
-            if (currentMissile != null){currentMissile.classList.toggle('hide')};
+            if (currentMissile != null){currentMissile.classList.remove('hide')};
             weaponEvent.innerHTML = option.innerHTML;
             weaponOptions.classList.toggle('show');
             console.log(option);
